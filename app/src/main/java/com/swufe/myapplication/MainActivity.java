4a08a990b5a174b6e8fc23a364aa4ce1a9bd4e81
zhuguodong199 <email@example.com>
2020-03-20 16:59:35 +0800

    事件绑定+计分程序

--- app/src/main/java/com/swufe/myapplication/MainActivity.java
+++ app/src/main/java/com/swufe/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.swufe.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+    TextView out; //TextView控件 类变量
+    EditText inp;//Edittext控件
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       out = findViewById(R.id.showText);//控件获取的初始化
+       inp = findViewById(R.id.inpText);
+
+       //Button btn = findViewById(R.id.btn1);
+       //btn.setOnClickListener(this);//当前类监听
+//        btn.setOnClickListener(new View.OnClickListener() {
+ //           @Override
+        //           public void onClick(View v) {//实现的接口匿名类监听
+//              Log.i("mail","onClick called....");
+//            String str = inp.getText().toString();
+//                out.setText("Hello" +str);
+//            }
+//        });
+    }
+
+   // @Override
+    //public void onClick(View v) {
+  //      Log.i("click","onClick.....");
+
+        //TextView tv = findViewById(R.id.showText);//没有定义类变量的时候用于重新获取对象
+
+        //EditText inp = findViewById(R.id.inpText);
+    //    String str = inp.getText().toString();
+    //    out.setText("Hello"  + str);
+
+  //  }
+    public void btnClick(View btn){//使用配置文件（最简便的方法，不用使用监听器）
+        Log.i("click", "btnClick called.....");
+        String str = inp.getText().toString();
+        out.setText("Hello"+" "+str);
     }
 }
---
+++ app/src/main/java/com/swufe/myapplication/ScoringActivity.java
@@ -0,0 +1,41 @@
+package com.swufe.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class ScoringActivity extends AppCompatActivity {
+    TextView score;//获取分数控件
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoring);
+
+        score=findViewById(R.id.score);//这里的id就是配置文件里的id
+    }
+    public void btnAdd1(View btn){
+        showScore(1);
+    }//这里取的名字最好对应布局视图里的id名，便于在给控件加方法的时候查找到
+    public void btnAdd2(View btn){
+        showScore(2);
+    }
+    public void btnAdd3(View btn){
+        showScore(3);
+    }
+    public void btnReset(View btn){
+        score.setText("0");
+    }
+
+    private void showScore(int inc){
+        Log.i("show","inc="+inc);
+        String oldScore = (String)score.getText();
+        int newScore = Integer.parseInt(oldScore) + inc;
+        score.setText(""+ newScore);
+
+
+    }
+
+}
